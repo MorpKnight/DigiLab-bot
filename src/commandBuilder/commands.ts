764166db0c3758b3184d31fe111b3ea8abc7cd0d
MorpKnight <christoffelsihombing@gmail.com>
2023-10-13 22:15:30 +0700

    add printInputGiven to commands

--- src/commandBuilder/commands.ts
+++ src/commandBuilder/commands.ts
@@ -1,5 +1,6 @@
 import { addRoleAll } from "../commands/addRoleAll";
 import { ping } from "../commands/ping";
+import { printInputGiven } from "../commands/printInputGiven";
 import { Command } from "./commandBuiler";
 
-export const Commands: Command[] = [addRoleAll, ping]
+export const Commands: Command[] = [addRoleAll, ping, printInputGiven]
